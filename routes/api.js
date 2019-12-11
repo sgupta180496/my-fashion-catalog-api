f7b58c6748d0bfd6bc8b1e6dfe599d2a28dd6084
sgupta180496 <53239577+sgupta180496@users.noreply.github.com>
2019-12-11 11:17:45 -0500

    changing endpoints

--- routes/api.js
+++ routes/api.js
@@ -6,13 +6,13 @@ router.get('/', function(req, res){
     res.send("Welcome!");
 });
 //get a list of items from database
-router.get('/catalog/:id', function (req, res, next) {
+router.get('/products/:id', function (req, res, next) {
     Item.findOne({ _id: req.params.id }).then(function (item) {
         res.send(item);
       });
 });
 
-router.get('/catalog', function (req, res, next) {
+router.get('/products', function (req, res, next) {
     Item.find({}).then(function(item){
         res.send(item);
     });
@@ -20,14 +20,14 @@ router.get('/catalog', function (req, res, next) {
 
 
 //add a new item in the catalog
-router.post('/addToCatalog', function (req, res, next) {
+router.post('/products', function (req, res, next) {
     Item.create(req.body).then(function (item) {
         res.send(item);
     }).catch(next);
 });
 
 //update an item in the database
-router.put('/UpdateCatalog/:id', function (req, res, next) {
+router.put('/products/:id', function (req, res, next) {
     Item.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function () {
         Item.findOne({ _id: req.params.id }).then(function (item) {
             res.send(item);
@@ -36,7 +36,7 @@ router.put('/UpdateCatalog/:id', function (req, res, next) {
 });
 
 //delete an item from the database
-router.delete('/deleteFromCatalog/:id', function (req, res, next) {
+router.delete('/products/:id', function (req, res, next) {
     //console.log(req.params.id);
     Item.findByIdAndRemove({ _id: req.params.id }).then(function (item) {
         res.send(item);
